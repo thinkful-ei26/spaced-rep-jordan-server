8afcaee5675e646dfcd644cf9d7f4f05af70159e
Jordan <jordanhaddadi@gmail.com>
2019-03-08 11:06:45 -0500

    jwt

--- server.js
+++ server.js
@@ -41,12 +41,12 @@ app.use(function(req, res, next) {
   });
 
 // Protect endpoints using JWT Strategy
-const jwtAuth = passport.authenticate('jwt', { session: false, failWithError: true });
+// const jwtAuth = passport.authenticate('jwt', { session: false, failWithError: true });
 
 // Mount routers
 app.use('/auth/users', usersRouter);
 app.use('/auth/login', authRouter);
-app.use('/api/questions', jwtAuth, questionsRouter);
+app.use('/api/questions', questionsRouter);
 
 
 function runServer(port = PORT) {
