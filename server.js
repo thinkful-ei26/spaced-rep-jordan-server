ceb2804707597331ceb8fc1e43cccff3aae9aa48
Jordan <jordanhaddadi@gmail.com>
2019-03-08 11:24:14 -0500

    blah

--- server.js
+++ server.js
@@ -34,9 +34,9 @@ app.use(
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
+    // res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,Authorization,authorization');
+    // res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,Authorization,authorization');
     next();
   });
 
